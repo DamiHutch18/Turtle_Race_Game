d2b50f105fb7b922da468ddb934dc966809447c7
Damian Hutchinson <vibingperson.dh@gmail.com>
2024-03-11 14:26:10 -0400

    New code

---
+++ Damian_programmingexercise_2-5.py
@@ -0,0 +1,14 @@
+distance6Hours = 0.0
+distance10Hours = 0.0
+distance15Hours = 0.0
+
+SPEED = 70
+
+distance6Hours = SPEED * 6
+distance10Hours = SPEED * 10
+distance15Hours = SPEED * 15
+
+print("The car wil travel the following disances:")
+print(distance6Hours, "miles in 6 hours.")
+print(distance10Hours, "miles in 10 hours.")
+print(distance15Hours, "miles in 15 hours.")
